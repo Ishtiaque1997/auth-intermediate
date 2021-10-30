f310de3f2580101906340233eee82c9a15679007
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-30 09:00:49 +0600

    context going on

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,14 @@
+import React,{createContext} from 'react';
+import useFirebase from './../hooks/useFirebase'
+const AuthContext=createContext();
+const AuthProvider = ({children}) => {
+ // const{children}=props;
+ const allContext=useFirebase();
+ return (
+  <AuthContext.Provider value='dynamic'>
+    {children}
+
+  </AuthContext.Provider>
+ );
+};
+export default AuthProvider;
